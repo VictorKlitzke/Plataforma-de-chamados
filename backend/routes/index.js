65eae499b333cbac8b25e0cf5db195e268a9c3ef
Victor Gabriel Miranda Klitzke <71741646+VictorKlitzke@users.noreply.github.com>
2024-11-29 15:36:15 -0400

    fix: autenticação de usaurio

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,11 +3,13 @@ const router = express.Router();
 
 const authControllers = require("../controllers/index");
 const authGet = require("../controllers/gets");
+
 const upload = require('../middleware/index');
+const auth = require("../middleware/auth");
 
 router.post("/login", authControllers.login)
-router.post("/registerCalled", upload.array('attachments', 10), authControllers.registerCalled)
+router.post("/registerCalled", upload.array('attachments', 10), auth, authControllers.registerCalled)
 
-router.get("/listCalled", authGet.getCalled)
+router.get("/listCalled", auth, authGet.getCalled)
 
 module.exports = router
